3bfd863d04ca7e6a4ce2af6894cae91c9b76c317
ishriyansh <ishreeyansh@gmail.com>
2020-05-07 17:31:30 +0530

    face detecttion works

---
+++ lib/face_painter.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FacePainter extends CustomPainter {
+  List<Rect> rect;
+  var imageFile;
+
+  FacePainter({@required this.rect, @required this.imageFile});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (imageFile != null) {
+      canvas.drawImage(imageFile, Offset.zero, Paint());
+    }
+
+    for (Rect rectangle in rect) {
+      canvas.drawRect(
+        rectangle,
+        Paint()
+          ..color = Colors.teal
+          ..strokeWidth = 6.0
+          ..style = PaintingStyle.stroke,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'face_painter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +12,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FLutter Face Dector',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  File pickedImage;
+  var imageFile;
+
+  List<Rect> rect =  new List<Rect>();
+  bool isFaceDetected = false;
+
+  void pickImage() async {
+  var awaitImage  =  await ImagePicker.pickImage(source : ImageSource.gallery);
+  imageFile = await awaitImage.readAsBytes();
+  imageFile = await decodeImageFromList(imageFile);
+
+  setState(() {
+    pickedImage = awaitImage;
+    imageFile = imageFile;
+  });
+
+  FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromFile(pickedImage);
+  final FaceDetector faceDetector = FirebaseVision.instance.faceDetector();
+
+  final List<Face> faces =  await faceDetector.processImage(firebaseVisionImage);
+  if (rect.length > 0) {
+    rect = new List<Rect>();
+  }
+  for (Face face in faces) {
+    rect.add(face.boundingBox);
+    final double rotY =
+        face.headEulerAngleY; // Head is rotated to the right rotY degrees
+    final double rotZ =
+        face.headEulerAngleZ; // Head is tilted sideways rotZ degrees
+    print('the rotation y is ' + rotY.toStringAsFixed(2));
+    print('the rotation z is '+ rotZ.toStringAsFixed(2));
+    }
+    setState(() {
+    isFaceDetected = true;
+    });
+
+
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Face Recognition"),
+        centerTitle: true,
+      ),
      
+      body: Column(
+        children: <Widget>[
+
+         Expanded(
+           child: imageFile != null ? FittedBox(
+              child: SizedBox(
+                width: imageFile.width.toDouble(),
+                height: imageFile.height.toDouble(),
+                child: CustomPaint(
+                  painter:
+                  FacePainter(rect: rect, imageFile: imageFile),
+                ),),):Container(),
+         ),
+          
+
+      FlatButton(
+        onPressed: (){
+          pickImage();
+        },
+        color: Colors.red,
+        child: Text("Pick Image"),
+      )
+
+
+        ],
+      ),
+  
+  
     );
   }
 }
